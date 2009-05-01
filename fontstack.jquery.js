d8c1f6ed9953355bb8fb2e433b1cef8579d222fb
Phil Oye <philoye@philoye.com>
2009-05-01 23:58:22 +1000

    Minor cleanup to js

--- fontstack.jquery.js
+++ fontstack.jquery.js
@@ -13,9 +13,9 @@ jQuery.fontstack = {
 
 	},
   
+
   identify: function(font_stacks) {
 
-    
     for (var i in font_stacks) {
       var stack = font_stacks[i];
       $.fontstack.identifyStack(stack);
@@ -23,48 +23,37 @@ jQuery.fontstack = {
     
   },
   
+
   identifyStack: function(stack) {
 
     var num_fonts = stack.length
     var last_resort = stack[num_fonts -1];
     var baseline = "monospace";
 
-    // I need to test if no generic specified at all, that's when we should add one.
+    // TODO: I need to test if no generic specified at all, that's when we should add one.
     if (last_resort == "monospace") {
       baseline = "sans-serif";
       stack.push(baseline);
     };
     
-		console.log(stack);
     for (var i=0; i<num_fonts; i++) {
       font = stack[i];
-      
+
       if ($.fontstack.testFont(font,baseline)) {
         console.log("Found " + font + ", add body class and break");
         $('body').addClass(font.replace( /\s/g, "").toLowerCase());
         break;
       }
-        
     }
-    
   },
 	
 	testFont: function(requested_font, baseline_font) {
-    console.log("testing " + requested_font + " against " + baseline_font);
 		
-		// Insert test div offscreen
 		$('body').prepend('<span id="font_tester" style="font-family:' + baseline_font + '; font-size:72px;left:-10000px;position:absolute;top:-10000px;visibility:hidden;">mmmmmmmmmmmmmmmmmmmmmmmmmmmml</span>');
 		
-		// Get baseline font width
 		var baseline_width = $('#font_tester').width();
-		
-		// Apply the requested font.
 		$('#font_tester').css('font-family', requested_font + ',' + baseline_font );
-
-		// Get the width again
 		var requested_width = $('#font_tester').width();
-		
-		// Remove our test span
 		$('#font_tester').remove();
 		
 		// If the dimensions change, the font is installed
